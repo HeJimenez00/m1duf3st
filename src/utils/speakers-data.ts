9b5f0d6e22bed2290e3bd394d99cad05beff2b58
heeector <141644350+HeJimenez00@users.noreply.github.com>
2024-04-10 02:18:32 -0600

    Add files via upload

---
+++ src/utils/speakers-data.ts
@@ -0,0 +1,42 @@
+export const speakers = [
+  {
+    id: 1,
+    name: "Rauno",
+    twitter: "raunofreiberg",
+    job: "UI/UX",
+    talk: "Learn UX basics",
+    description:
+      "¿Qué tan rápido debe cargar su sitio web? ¿Quizás 5 o 6 segundos? ¿Cómo lo sabes realmente? ¿Por qué tu web va lenta? Cometemos muchos errores al desarrollar sitios con JavaScript. En esta charla, exploraremos algunos de los problemas más comunes y no tan comunes en los sitios web. También aprenderemos cómo podemos monitorear los datos reales provenientes de usuarios reales para tomar mejores decisiones.",
+    state: "US",
+    image:
+      "https://pbs.twimg.com/profile_images/1525606643794427905/17-x8e9o_400x400.jpg",
+    date: "09:00",
+  },
+  {
+    id: 2,
+    name: "Guillermo Rauch",
+    twitter: "rauchg",
+    job: "CEO Vercel",
+    talk: "El futuro de la Web 🤯",
+    description:
+      "¿Qué tan rápido debe cargar su sitio web? ¿Quizás 5 o 6 segundos? ¿Cómo lo sabes realmente? ¿Por qué tu web va lenta? Cometemos muchos errores al desarrollar sitios con JavaScript. En esta charla, exploraremos algunos de los problemas más comunes y no tan comunes en los sitios web. También aprenderemos cómo podemos monitorear los datos reales provenientes de usuarios reales para tomar mejores decisiones.",
+    state: "US",
+    image:
+      "https://pbs.twimg.com/profile_images/1731856122624249856/qUqIRpIg_400x400.jpg",
+    imageAgenda: "/public/agenda/rauch.png",
+    date: "10:00",
+  },
+  {
+    id: 3,
+    name: "ThePrime",
+    twitter: "ThePrimeagen",
+    job: "Ex-SWE Netflix",
+    talk: "HTMX IS BIGGEST 🔥",
+    description:
+      "OOOOOOMG!!! ¿Quizás 5 o 6 segundos? ¿Cómo lo sabes realmente? ¿Por qué tu web va lenta? Cometemos muchos errores al desarrollar sitios con JavaScript. En esta charla, exploraremos algunos de los problemas más comunes y no tan comunes en los sitios web. También aprenderemos cómo podemos monitorear los datos reales provenientes de usuarios reales para tomar mejores decisiones.",
+    state: "US",
+    image:
+      "https://pbs.twimg.com/profile_images/1759330620160049152/2i_wkOoK_400x400.jpg",
+    date: "11:00",
+  },
+];
